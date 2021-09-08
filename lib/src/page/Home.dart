42359162f9752b850fd7ae0311f77a9401634f6d
Martin Jimenez <majiar16@gmail.com>
2021-09-08 17:55:18 -0500

    style google map

--- lib/src/page/Home.dart
+++ lib/src/page/Home.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class Home extends StatefulWidget {
   @override
@@ -7,13 +10,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _mapStyle;
+  var mapController;
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Entregar'),
       ),
-      body: _map(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 15,
+          ),
+          Text("mapa"),
+          SizedBox(
+            height: 15,
+          ),
+          Container(height: 300, child: _map()),
+        ],
+      ),
     );
   }
 
@@ -28,11 +53,14 @@ class _HomeState extends State<Home> {
 
   Widget _map() {
     return GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: LatLng(10.909075, -74.802325),
-        zoom: 12,
-      ),
-      markers: _createMarkers(),
-    );
+        initialCameraPosition: CameraPosition(
+          target: LatLng(10.909075, -74.802325),
+          zoom: 12,
+        ),
+        markers: _createMarkers(),
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+          mapController.setMapStyle(_mapStyle);
+        });
   }
 }
