68c1ba53420399ddbf64c9e17e9f1109d4d5dbdd
Martin Jimenez <majiar16@gmail.com>
2021-09-08 16:27:55 -0500

    add google maps

--- lib/src/page/Home.dart
+++ lib/src/page/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,7 +13,26 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Entregar'),
       ),
-      body: Text('Holaaaa!!!'),
+      body: _map(),
+    );
+  }
+
+  Set<Marker> _createMarkers() {
+    var tmp = Set<Marker>();
+    tmp.add(Marker(
+      markerId: MarkerId("value"),
+      position: LatLng(10.909075, -74.802325),
+    ));
+    return tmp;
+  }
+
+  Widget _map() {
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(10.909075, -74.802325),
+        zoom: 12,
+      ),
+      markers: _createMarkers(),
     );
   }
 }
